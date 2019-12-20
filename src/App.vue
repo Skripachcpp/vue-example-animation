30830728012a5b8ceb4be48ba7ffc08d08e3be21
Artem Ruzhilov <skripach.cpp@gmail.com>
2019-12-20 14:16:29 +0300

    example 1

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <Example1 />
+  </div>
+</template>
+
+<script>
+import Example1 from "./components/Example1";
+
+export default {
+  name: "app",
+  components: {
+    Example1
+  }
+};
+</script>
+
+<style lang="scss">
+html {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Example1/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="style.box">
+    <div :class="style.left"><div :class="style.rectangle"></div></div>
+    <div :class="style.right"><div :class="style.round"></div></div>
+  </div>
+</template>
+
+<script>
+import style from "./style.module.scss";
+export default {
+  name: "Example1",
+  computed: {
+    style: () => style
+  }
+};
+</script>
+
+<style scoped></style>
